7ae3fdecf8bc3d6acd02eaaed13b6ffffb208689
ycm <ycm@github.com>
2024-12-01 04:00:32 -0800

    safe <CR> in

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -10,7 +10,6 @@ return {
     },
     {
         'hrsh7th/nvim-cmp',
-
         config = function()
             local cmp = require('cmp')
             require('luasnip.loaders.from_vscode').lazy_load()
@@ -29,7 +28,18 @@ return {
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["<CR>"] = cmp.mapping({
+                        i = function(fallback)
+                            if cmp.visible() and cmp.get_active_entry() then
+                                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                            else
+                                fallback()
+                            end
+                        end,
+                        s = cmp.mapping.confirm({ select = true }),
+                        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+                    }),
+
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
